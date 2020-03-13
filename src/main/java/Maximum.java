32236e804b472ab42460da4edbdfe55296fc57cd
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-03-13 18:19:39 +0530

    ADDED: Method to find maximum from float.

--- src/main/java/Maximum.java
+++ src/main/java/Maximum.java
@@ -3,10 +3,20 @@ public class Maximum {
     public Integer maximumInteger (Integer[] maxInteger)
     {
         Integer maximum = 0;
-        for(Integer i: maxInteger)
+        for(Integer integer: maxInteger)
         {
-            if(i.compareTo(maximum)>0)
-                maximum=i;
+            if(integer.compareTo(maximum)>0)
+                maximum=integer;
+        }
+        return maximum;
+    }
+    public Float maximumFloat (Float[] maxFloat)
+    {
+        Float maximum = 0.0f;
+        for(Float floatValue: maxFloat)
+        {
+            if(floatValue.compareTo(maximum)>0)
+                maximum=floatValue;
         }
         return maximum;
     }
