41347a1b322ae058284ceff293c6c85f416b9176
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-03-13 18:40:50 +0530

    ADDED: Method to find maximum of strings.

--- src/main/java/Maximum.java
+++ src/main/java/Maximum.java
@@ -20,4 +20,14 @@ public class Maximum {
         }
         return maximum;
     }
+    public String maximumString (String[] maxString)
+    {
+        String maximum="";
+        for(String i: maxString)
+        {
+            if(i.compareTo(maximum)>0)
+                maximum=i;
+        }
+        return maximum;
+    }
 }
