e8ddee18a4aa8d7110746239d2a17fc3e960603d
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-03-14 17:01:40 +0530

    ADDED: Method to print maximum generic element.

--- src/main/java/Maximum.java
+++ src/main/java/Maximum.java
@@ -1,21 +1,20 @@
 //GENERIC CLASS
 public class Maximum<E extends Comparable> {
-    private  E firstValue;
-    private  E secondValue;
-    private  E thirdValue;
+    private E firstValue;
+    private E secondValue;
+    private E thirdValue;
 
     //PARAMETERIZED CONSTRUCTOR
-    public Maximum (E firstValue , E secondValue, E thirdValue) {
+    public Maximum(E firstValue, E secondValue, E thirdValue) {
         this.firstValue = firstValue;
         this.secondValue = secondValue;
         this.thirdValue = thirdValue;
     }
 
     //METHOD TO COMPARE GENERIC PARAMETERS AND RETURN MAXIMUM
-    public  <E extends Comparable> E testMaximum (E firstValue , E secondValue, E thirdValue)
-    {
+    public <E extends Comparable> E testMaximum(E firstValue, E secondValue, E thirdValue) {
         if (firstValue.compareTo(secondValue) >= 0 && firstValue.compareTo(thirdValue) >= 0) {
-            return (E)firstValue;
+            return (E) firstValue;
         } else if (secondValue.compareTo(firstValue) >= 0 && secondValue.compareTo(thirdValue) >= 0) {
             return (E) secondValue;
         } else {
@@ -24,7 +23,13 @@ public class Maximum<E extends Comparable> {
     }
 
     //METHOD OVERRIDEN TO INITIALISE PARAMETERS OF METHOD
-    public E testMaximum (){
+    public E testMaximum() {
+        printMax(testMaximum(firstValue, secondValue, thirdValue));
         return testMaximum(firstValue, secondValue, thirdValue);
+    }
+
+    //METHOD TO PRINT MAXIMUM GENERIC ELEMENT
+    public void printMax(E max) {
+        System.out.println("Maximum Value : " + max);
     }
 }
